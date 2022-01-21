705cab6273b33b633f770130f2d996085d4954ad
twilkhoo <tejaswilkhoo@hotmail.com>
2022-01-20 19:35:49 -0500

    mediapipe hand tracking

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -1,13 +1,38 @@
+from tkinter import N
 import cv2
 import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands() # keep default confidence levels
+mpDraw = mp.solutions.drawing_utils
 
+pTime = 0
+cTime = 0
 
 while True:
     success, img = cap.read()  
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+    
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                print(id,lm)
+                cv2.putText(img, str(lm), (10,10), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 255), 3)
+
+
+            mpDraw.draw_landmarks(img, handLms)
+
+    
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int (fps)), (10,70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 255), 3)
 
 
 
