79408d93c7916cb04ab82ff05795f5478e1bb7c2
twilkhoo <tejaswilkhoo@hotmail.com>
2022-01-23 22:25:45 -0500

    contact of index with thumb

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -5,10 +5,6 @@ import mediapipe as mp
 import time
 from google.protobuf.json_format import MessageToDict
 
-# for i in range (3000000000000000):
-#     time.sleep(0.5)
-#     print(i)
-
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands() # keep default confidence levels
@@ -26,28 +22,29 @@ while True:
     if results.multi_hand_landmarks: # if a hand is in the frame
         for handLms in results.multi_hand_landmarks: # for every hand in the frame
 
-            hand_label = 'none'
-            for idx, hand_handedness in enumerate(results.multi_handedness): # find what hands are in the frame
-                handedness_dict = MessageToDict(hand_handedness)
-                hand_label_opp = handedness_dict['classification'][0]['label']
-                hand_label = 'Left' if hand_label_opp == 'Right' else 'Right'
+            for id, lm in enumerate(handLms.landmark): # for every landmark id
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                index_x = 0
+                index_y = 0
+                thumb_x = 0
+                thumb_y = 0
 
-                for id, lm in enumerate(handLms.landmark): # for every landmark id
-                    h, w, c = img.shape
-                    cx, cy = int(lm.x*w), int(lm.y*h)
+                if id == 4:
+                    cv2.putText(img, "RThumb: " + str(cx) + ", " + str(cy), (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
+                    thumb_x = cx;
+                    thumb_y = cy;
 
-                    if id == 4:
-                        cv2.putText(img, "RThumb: " + str(cx) + ", " + str(cy), (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
-                    if id == 8:
-                        cv2.putText(img, "RIndex: " + str(cx) + ", " + str(cy), (10,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
-                    if id == 12:
-                        cv2.putText(img, "RMiddle: " + str(cx) + ", " + str(cy), (10,150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
-                    if id == 16:
-                        cv2.putText(img, "RRing: " + str(cx) + ", " + str(cy), (10,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
-                    if id == 20:
-                        cv2.putText(img, "RPinky: " + str(cx) + ", " + str(cy), (10,250), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
-                    
-                    cv2.putText(img, hand_label, (10,300), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
+                if id == 8:
+                    cv2.putText(img, "RIndex: " + str(cx) + ", " + str(cy), (10,150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
+                    index_x = cx;
+                    index_y = cy;
+
+                if ((((thumb_x - index_x) ** 2) + ((thumb_y - index_y) ** 2)) ** (1/2) < 3000000):
+                    #cv2.putText(img, "touch", (10,250), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
+                    print('touch')
+
+
 
             mpDraw.draw_landmarks(img, handLms)
 
@@ -62,3 +59,66 @@ while True:
     # img_flip = cv2.flip(img, 1)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+
+
+
+
+
+
+
+# the code below is the implementation for two hands 
+# while True:
+#     success, img = cap.read()  
+#     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#     results = hands.process(imgRGB)
+#     #print(results.multi_hand_landmarks)
+    
+#     if results.multi_hand_landmarks: # if a hand is in the frame
+#         for handLms in results.multi_hand_landmarks: # for every hand in the frame
+
+#             hand_label = 'none'
+#             for idx, hand_handedness in enumerate(results.multi_handedness): # find what hands are in the frame
+#                 handedness_dict = MessageToDict(hand_handedness)
+#                 hand_label_opp = handedness_dict['classification'][0]['label']
+#                 hand_label = 'Left' if hand_label_opp == 'Right' else 'Right'
+                
+#                 if hand_label == 'Left':
+#                     cv2.putText(img, "Left", (10,300), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
+#                     for id, lm in enumerate(handLms.landmark): # for every landmark id
+#                         h, w, c = img.shape
+#                         cx, cy = int(lm.x*w), int(lm.y*h)
+
+#                         if id == 4:
+#                             cv2.putText(img, "LThumb: " + str(cx) + ", " + str(cy), (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
+
+#                         if id == 8:
+#                             cv2.putText(img, "LIndex: " + str(cx) + ", " + str(cy), (10,150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
+
+
+
+#                 if hand_label == 'Right':
+#                     cv2.putText(img, "Right", (100,300), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
+#                     for id, lm in enumerate(handLms.landmark): # for every landmark id
+#                         h, w, c = img.shape
+#                         cx, cy = int(lm.x*w), int(lm.y*h)
+#                         if id == 4:
+#                             cv2.putText(img, "RThumb: " + str(cx) + ", " + str(cy), (10,250), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
+
+#                         if id == 8:
+#                             cv2.putText(img, "RIndex: " + str(cx) + ", " + str(cy), (10,350), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
+
+
+
+#             mpDraw.draw_landmarks(img, handLms)
+
+    
+#     cTime = time.time()
+#     fps = 1 / (cTime - pTime)
+#     pTime = cTime
+
+#     cv2.putText(img, "FPS:" + str(int(fps)), (10,450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 3)
+
+
+#     # img_flip = cv2.flip(img, 1)
+#     cv2.imshow("Image", img)
+#     cv2.waitKey(1)
